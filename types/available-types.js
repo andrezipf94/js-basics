7877a1d41ca0f9932812f451e410e911da489f7e
Andre Luis Zipf <andre.zipf@westwing.com.br>
2023-01-04 12:46:46 -0300

    Added available types

---
+++ types/available-types.js
@@ -0,0 +1,20 @@
+// String
+typeof 'John Doe';
+
+// Number
+typeof 10;
+
+// Boolean
+typeof false;
+
+// Object
+typeof null;
+typeof new Date();
+typeof /[A-Za-z$_]/;
+typeof [];
+
+// Symbol
+typeof new Symbol();
+
+// Undefined
+typeof undefined;
